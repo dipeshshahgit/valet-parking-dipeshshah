cdb7cf66ef9868230c478defc55b6260c3486dfe
Dipesh Shah <dipesh.shah@storms.com>
2021-05-06 01:52:05 +0800

    - fixing mvn path - fixed warnings while running maven

--- run-program.sh
+++ run-program.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 arg1=$1
-echo "executing mvn -q clean package"
-echo "executing junit Tests"
-~/Downloads/apache-maven-3.6.3/bin/mvn -q clean package
-echo "Done";
-echo "=================================================================";
-echo "\nExecuting Program ValetParking\n"
-if [ -z "$1" ]; then
+echo "executing mvn clean package -Dmaven.test.skip=true"
+mvn clean package -Dmaven.test.skip=true
+if [ $? -eq 0 ]; then
+    echo "------------------------------------------------------------------------"
+    echo "Packaged target/ValetParkingApplication-1.0-SNAPSHOT.jar successfully."
+    echo "------------------------------------------------------------------------"
+    echo "Executing Program ValetParking with sample: src/main/resources/input-data.txt"
+    echo "\"java -jar target/ValetParkingApplication-1.0-SNAPSHOT.jar src/main/resources/input-data.txt\"\n"
+    if [ -z "$1" ]; then
         java -jar target/ValetParkingApplication-1.0-SNAPSHOT.jar src/main/resources/input-data.txt
+        echo "------------------------------------------------------------------------"
+        echo "to execute Junit tests, execute command \"mvn test\""
+        echo "------------------------------------------------------------------------"
         exit 1
-else
+    else
         java -jar target/ValetParkingApplication-1.0-SNAPSHOT.jar $arg1
+    fi
+else
+    echo "Failed, please try running from cli"
 fi
